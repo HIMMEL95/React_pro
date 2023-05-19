3a4ab97808962411fdfaf29e02a4282f818e750d
HIMMEL <bbluesky7738@gmail.com>
2023-05-19 17:32:30 +0900

    chang commit

--- react-redux-tutorial/src/containers/TodosContainer.js
+++ react-redux-tutorial/src/containers/TodosContainer.js
@@ -2,22 +2,37 @@ import { changeInput, insert, toggle, remove } from '../modules/todos';
 import Todos from '../components/Todos';
 import { connect } from 'react-redux';
 
-const TodosContainer = ({changeInput, insert, toggle, remove}) => {
-    return (
-        <Todos todo={changeInput} onSubmit={changeInput} />
-    );
+const TodosContainer = ({
+  input,
+  todos,
+  changeInput,
+  insert,
+  toggle,
+  remove,
+}) => {
+  return (
+    <Todos
+      input={input}
+      todos={todos}
+      onChangeInput={changeInput}
+      onInsert={insert}
+      onToggle={toggle}
+      onRemove={remove}
+    />
+  );
 };
 
 export default connect(
-    (state) => ({
-        input: state.todos.input,
-    }),
-    {
-        changeInput,
-        insert,
-        toggle,
-        remove
-    }
-)
+  ({ todos }) => ({
+    input: todos.input,
+    todos: todos.todos,
+  }),
+  {
+    changeInput,
+    insert,
+    toggle,
+    remove,
+  },
+)(TodosContainer);
 
 // export default TodosContainer;
