d991b67fb3c0b7552dfed5852fd31bb2b707714d
Himmel <bbluesky7738@gmail.com>
2023-05-23 22:31:40 +0900

    start project

---
+++ per/src/main/frontend/src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { legacy_createStore as createStore } from "redux";
+import rootReducer from "./modules";
+import { Provider } from "react-redux";
+
+const store = createStore(rootReducer);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
